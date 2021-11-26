3201c87b2a81b3d70e5c5efc5df16ed5a603967a
Robson Souza <robson.silvadesuoza@gmail.comgit config --listgit config --global user.name Robson>
2021-11-26 11:32:37 -0300

    Projeto Todo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div class="" id="app">
+    <div class="container grid-xs py-2">
+      <img class="img-responsive img-logo" src="@/assets/logo.jpg" alt="Logomarca Alura">
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Adicione uma nova tarefa aqui">
+          <button class="btn">Adicionar</button>
+        </div>       
+      </form>
+      <div class="todo-list">
+        <todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t"/>
+          </div>
+      </div>
+      </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from "./components/Todo";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+    components: {Todo},
+    data(){
+    return { todos: [], todo: { checked: false } };
+  },
+  methods: {
+    addTodo(todo) {
+      todo.id = Date.now();
+      this.todos.push(todo);
+      this.todo = { checked: false };
+    },
+
+    toggleTodo(todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+      if (index > -1) {
+        const checked = !this.todos[index].checked
+        this.$set(this.todos, index, {...this.todos[index], checked});
+      }
+    },
+
+    removeTodo (todo) {
+            const index = this.todos.findIndex(item => item.id === todo.id)
+          if (index > -1) {
+            this.$delete(this.todos, index);
+          }
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.img-logo {
+  max-width: 100px;
+  max-height: 100px;
+  margin: 100px auto;
+  margin-bottom: 50px;
+}
+
+.todo-list {
+  padding-top: 2rem;
+}
+
+.btn {
+      background: #297fb8;
+      color: #fff;
+		font-weight: bold;
+		font-size: 15px;
+		font-family: Arial, Helvetica, sans-serif;
 }
 </style>
+
